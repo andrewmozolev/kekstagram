1235ad1062c32ade00d2e5c310301c5bfeed8ce1
mozolev <avmozolev@yandex.ru>
2016-11-22 16:12:53 +0300

    add async like

--- src/js/picture/picture.js
+++ src/js/picture/picture.js
@@ -14,7 +14,10 @@ var Picture = function(data, container, index) {
   BaseComponent.call(this, this.element, this.container);
 
   this.showPicture = this.showPicture.bind(this);
+  this.updateLikes = this.updateLikes.bind(this);
+
   this.addEvent(this.element, 'click', this.showPicture);
+  this.addEvent(window, 'changeLikes', this.updateLikes);
 };
 
 utils.inherit(Picture, BaseComponent);
@@ -22,6 +25,10 @@ utils.inherit(Picture, BaseComponent);
 Picture.prototype.showPicture = function(evt) {
   evt.preventDefault();
   gallery.show(this.index);
+};
+
+Picture.prototype.updateLikes = function() {
+  this.element.querySelector('.picture-likes').textContent = this.data.getLikes();
 };
 
 module.exports = Picture;
